4024f1d57fc53b08389f2c21e6e684a3ae5a9bd5
pubgpro1488666-cyber <pubgpro1488666@gmail.com>
2026-06-30 21:55:13 +0300

    Add ModSettings class for client configuration

---
+++ src/client/java/com/example/client/ModSettings.java
@@ -0,0 +1,8 @@
+package com.example.client;
+
+public class ModSettings {
+    public static boolean hudEnabled = true;
+    public static boolean armorHudEnabled = true;
+    public static boolean keyStrokesEnabled = true;
+    public static boolean customMusicEnabled = false;
+}
